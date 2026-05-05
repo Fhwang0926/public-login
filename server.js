9d2b417d4ec3e96309ee8eca7a00065800b7cbde
donghwa <donghwa.dev@gmail.com>
2026-05-05 22:19:42 +0900

    fix-expose

--- server.js
+++ server.js
@@ -194,8 +194,8 @@ async function main() {
   initSchemaAndSeed();
 
   const PORT = Number(process.env.PORT) || 5000;
-  app.listen(PORT, "127.0.0.1", () => {
-    console.log(`http://127.0.0.1:${PORT}`);
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`http://0.0.0.0:${PORT}`);
   });
 }
 
